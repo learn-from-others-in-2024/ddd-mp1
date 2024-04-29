70d9c5366e0ae2a9bd8566555e94d7d9283fe767
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-04-29 13:47:31 +0530

    Changes

--- src/a2products/catalog/productRepositoryImplementation.go
+++ src/a2products/catalog/productRepositoryImplementation.go
@@ -1,5 +1,7 @@
 package catalog
 
+import "strconv"
+
 // productRepository is the implementation of the repository
 type productRepositoryImplementation struct {
 }
@@ -13,8 +15,8 @@ func NewProductRepositoryImplementation() ProductRepository {
 func (p productRepositoryImplementation) GetAllProducts() []Product {
 	var products []Product
 
-	for i := 0; i < 10; i++ {
-		products = append(products, NewProduct(string(i), 5644.8, "Product # "+string(i)))
+	for i := 1; i <= 10; i++ {
+		products = append(products, NewProduct(strconv.Itoa(i), 5644.8, "Product #"+strconv.Itoa(i)))
 	}
 
 	return products
--- src/a2products/main.go
+++ src/a2products/main.go
@@ -11,7 +11,7 @@ func main() {
 	sells := cat.NewSells(cat.NewProductRepositoryImplementation())
 	products := sells.ListProducts()
 
-	fmt.Println("List of products:")
+	fmt.Println("List of products:\n")
 
 	for _, v := range products {
 		fmt.Printf("%s - %s - %f\n \n", v.Id, v.Name, v.Price)
